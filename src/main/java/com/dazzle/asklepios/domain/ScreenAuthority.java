c9af2fec9799d6ea40f4c94c0e764c6e02f25d88
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-09-23 14:27:31 +0300

    role manegemant

---
+++ src/main/java/com/dazzle/asklepios/domain/ScreenAuthority.java
@@ -0,0 +1,36 @@
+package com.dazzle.asklepios.domain;
+
+import com.dazzle.asklepios.domain.enumeration.Operation;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "screen_authority")
+public class ScreenAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull(message = "screen must not be null")
+    @Column(name = "screen", nullable = false, length = 100)
+    private String screen;
+
+    @NotNull(message = "operation must not be null")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "operation", nullable = false, length = 50)
+    private Operation operation;
+
+    @NotNull(message = "authorityName must not be null")
+    @Column(name = "authority_name", nullable = false, length = 100)
+    private String authorityName;
+}
+
