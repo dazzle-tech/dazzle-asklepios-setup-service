458c3f21bd6c60ec48b4b986e41a699f2caa37c1
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2025-10-06 17:58:41 +0300

    Revert "FV"

--- src/main/java/com/dazzle/asklepios/config/SecurityConfiguration.java
+++ src/main/java/com/dazzle/asklepios/config/SecurityConfiguration.java
@@ -36,7 +36,6 @@ public class SecurityConfiguration {
                 // prettier-ignore
                 authz
                     .requestMatchers(mvc.pattern("/setup/api/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/setup-service/v3/api-docs/**")).hasAuthority(AuthoritiesConstants.ADMIN)
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
                     .requestMatchers(mvc.pattern("/api/**")).permitAll()
                     .requestMatchers(mvc.pattern("/v3/api-docs/**")).hasAuthority(AuthoritiesConstants.ADMIN)
