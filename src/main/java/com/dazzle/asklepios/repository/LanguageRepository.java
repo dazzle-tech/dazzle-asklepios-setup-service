426718fec677d2a884e8d5e59957dc577de076b1
bushramashaiekh <bushramashaie5@gmail.com>
2025-10-16 11:46:52 +0300

    Languages and Translation

---
+++ src/main/java/com/dazzle/asklepios/repository/LanguageRepository.java
@@ -0,0 +1,17 @@
+package com.dazzle.asklepios.repository;
+
+import com.dazzle.asklepios.domain.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+    Optional<Language> findByLangKey(String langKey);
+
+    boolean existsByLangKey(String langKey);
+
+    void deleteByLangKey(String langKey);
+}
