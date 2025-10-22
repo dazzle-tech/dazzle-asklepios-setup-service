57d529e7912aecc195579cc926317db660ad8a19
bushramashaiekh <bushramashaie5@gmail.com>
2025-10-22 14:36:14 +0300

    access screen by path while user signed out

--- src/main/java/com/dazzle/asklepios/config/SecurityConfiguration.java
+++ src/main/java/com/dazzle/asklepios/config/SecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthen
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
@@ -31,27 +32,28 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authz ->
-                // prettier-ignore
-                authz
-                    .requestMatchers(mvc.pattern("/setup/api/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
-                    .requestMatchers(mvc.pattern("/api/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/v3/api-docs/**")).authenticated()
-                    .requestMatchers(mvc.pattern("/management/health")).permitAll()
-                    .requestMatchers(mvc.pattern("/management/health/**")).permitAll()
-                    .requestMatchers(mvc.pattern("/management/info")).permitAll()
-                    .requestMatchers(mvc.pattern("/management/prometheus")).permitAll()
-                    .requestMatchers(mvc.pattern("/management/**")).hasAuthority(AuthoritiesConstants.ADMIN)
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .exceptionHandling(exceptions ->
-                exceptions
-                    .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz ->
+                        // prettier-ignore
+                        authz
+                                .requestMatchers(mvc.pattern("/setup/api/**")).permitAll()
+                                .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
+                                .requestMatchers(new AntPathRequestMatcher("/api/setup/facility", "GET")).permitAll()
+                                .requestMatchers(mvc.pattern("/api/**")).authenticated()
+                                .requestMatchers(mvc.pattern("/v3/api-docs/**")).authenticated()
+                                .requestMatchers(mvc.pattern("/management/health")).permitAll()
+                                .requestMatchers(mvc.pattern("/management/health/**")).permitAll()
+                                .requestMatchers(mvc.pattern("/management/info")).permitAll()
+                                .requestMatchers(mvc.pattern("/management/prometheus")).permitAll()
+                                .requestMatchers(mvc.pattern("/management/**")).hasAuthority(AuthoritiesConstants.ADMIN)
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptions ->
+                        exceptions
+                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
         return http.build();
     }
 
