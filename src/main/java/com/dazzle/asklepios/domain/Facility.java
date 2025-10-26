8a7fc3739b4f5b495a108a8f8439e49c795e1cfb
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-10-26 21:42:34 +0200

    delete  implementation 'com.vladmihalcea:hibernate-types-60:2.21.1'

--- src/main/java/com/dazzle/asklepios/domain/Facility.java
+++ src/main/java/com/dazzle/asklepios/domain/Facility.java
@@ -3,8 +3,14 @@ package com.dazzle.asklepios.domain;
 import com.dazzle.asklepios.domain.enumeration.Currency;
 import com.dazzle.asklepios.domain.enumeration.FacilityType;
 import com.dazzle.asklepios.domain.enumeration.FacilityTypeConverter;
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +22,6 @@ import lombok.Setter;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
