21b5c8250c53a76cc425dbe93ce0a0764b8f7b03
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2025-11-17 09:43:34 +0200

    Remove constraint exception rethrow in create method
    Remove rethrowing of constraint exceptions in create method.

--- src/main/java/com/dazzle/asklepios/service/VaccineDosesService.java
+++ src/main/java/com/dazzle/asklepios/service/VaccineDosesService.java
@@ -67,7 +67,6 @@ public class VaccineDosesService {
             return saved;
         } catch (DataIntegrityViolationException | JpaSystemException constraintException) {
             handleConstraintViolation(constraintException);
-            throw constraintException;
         }
     }
 
