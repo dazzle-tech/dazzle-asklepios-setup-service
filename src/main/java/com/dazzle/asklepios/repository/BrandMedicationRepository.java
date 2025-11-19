9e7df0c931a5a91fe62e0df8f1f8288fc02da6b0
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2025-11-19 11:00:39 +0200

    simplify query of findAllBrandMedicationsSharingActiveIngredients

--- src/main/java/com/dazzle/asklepios/repository/BrandMedicationRepository.java
+++ src/main/java/com/dazzle/asklepios/repository/BrandMedicationRepository.java
@@ -10,25 +10,34 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface BrandMedicationRepository  extends JpaRepository<BrandMedication, Long> {
-Page<BrandMedication> findByNameContainsIgnoreCase(String name, Pageable pageable);
-Page<BrandMedication> findByManufacturerContainsIgnoreCase(String manufacturer, Pageable pageable);
-Page<BrandMedication> findByDosageFormContainsIgnoreCase(String dosageForm, Pageable pageable);
-Page<BrandMedication> findByUsageInstructionsContainsIgnoreCase(String usageInstructions, Pageable pageable);
-Page<BrandMedication> findByRoaContainsIgnoreCase(String roa, Pageable pageable);
-Page<BrandMedication> findByExpiresAfterOpening(Boolean expiresAfter, Pageable pageable);
-Page<BrandMedication> findByUseSinglePatient(Boolean useSinglePatient, Pageable pageable);
-Page<BrandMedication> findByIsActive(Boolean isActive, Pageable pageable);
-    @Query("""
-    SELECT DISTINCT bai2.brandMedication
-    FROM BrandMedicationActiveIngredient bai1
-    JOIN BrandMedicationActiveIngredient bai2
-      ON bai1.activeIngredients = bai2.activeIngredients
-    WHERE bai1.brandMedication.id = :brandId
-      AND bai2.brandMedication.id <> :brandId
-    """)
-    List<BrandMedication> findAllBrandMedicationsSharingActiveIngredients(Long brandId);
+public interface BrandMedicationRepository extends JpaRepository<BrandMedication, Long> {
+    Page<BrandMedication> findByNameContainsIgnoreCase(String name, Pageable pageable);
 
+    Page<BrandMedication> findByManufacturerContainsIgnoreCase(String manufacturer, Pageable pageable);
+
+    Page<BrandMedication> findByDosageFormContainsIgnoreCase(String dosageForm, Pageable pageable);
+
+    Page<BrandMedication> findByUsageInstructionsContainsIgnoreCase(String usageInstructions, Pageable pageable);
+
+    Page<BrandMedication> findByRoaContainsIgnoreCase(String roa, Pageable pageable);
+
+    Page<BrandMedication> findByExpiresAfterOpening(Boolean expiresAfter, Pageable pageable);
+
+    Page<BrandMedication> findByUseSinglePatient(Boolean useSinglePatient, Pageable pageable);
+
+    Page<BrandMedication> findByIsActive(Boolean isActive, Pageable pageable);
+
+    @Query("""
+              SELECT DISTINCT bai.brandMedication
+            FROM BrandMedicationActiveIngredient bai
+            WHERE bai.activeIngredients IN (
+            SELECT bai2.activeIngredients
+            FROM BrandMedicationActiveIngredient bai2
+             WHERE bai2.brandMedication.id = :brandId
+             )
+            AND bai.brandMedication.id <> :brandId
+                        """)
+    List<BrandMedication> findAllBrandMedicationsSharingActiveIngredients(Long brandId);
 
 
 }
