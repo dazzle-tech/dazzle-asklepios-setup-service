8ead7d3de33675f6535d15a04f583c637e0f3595
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-11-26 16:01:09 +0200

    enum for family member in patient

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/FamilyMemberCategory.java
@@ -0,0 +1,6 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum FamilyMemberCategory {
+    ADULT,
+    MINOR
+}
---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/RelationType.java
@@ -0,0 +1,30 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+/**
+ * Relation types based on LOV value_code in RELATION lov.
+ */
+public enum RelationType {
+    SPOUSE,
+    FRIEND,
+    SON,
+    MOTH,
+    FATH,
+    DAUG,
+    MOTHLAW,
+    FATHLAW,
+    SONLAW,
+    DAUGHLOW,
+    STEPFATH,
+    STEPMOM,
+    STEPDAUGHT,
+    STEPBRO,
+   STEPSON,
+   GRANDSON,
+    GRANDFATH,
+    GRANDAUGHT,
+    GRANDMA,
+    COUS,
+    BRO,
+    SIS
+}
+
