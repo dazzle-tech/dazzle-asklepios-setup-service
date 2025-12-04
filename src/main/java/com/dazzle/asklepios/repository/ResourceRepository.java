df7ef0f22c1e526238b97fec85ed4c8cf5eb3072
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2025-12-04 23:57:43 +0200

    v1

--- src/main/java/com/dazzle/asklepios/repository/ResourceRepository.java
+++ src/main/java/com/dazzle/asklepios/repository/ResourceRepository.java
@@ -1,11 +1,12 @@
 package com.dazzle.asklepios.repository;
 
-
 import com.dazzle.asklepios.domain.Resource;
- import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
@@ -13,8 +14,8 @@ public interface ResourceRepository extends JpaRepository<Resource, Long> {
     Page<Resource> findAll(Pageable pageable);
 
     Page<Resource> findByResourceType(String resourceType, Pageable pageable);
+
     Page<Resource> findByResourceTypeAndIsActiveTrue(String resourceType, Pageable pageable);
 
-
-
+    List<Resource> findByResourceTypeAndIsActiveTrue(String resourceType);
 }
