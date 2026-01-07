0486de3cfe923ddb50e85b11c798c9e40eca5173
bushramashaiekh <60824341+bushramashaiekh@users.noreply.github.com>
2026-01-07 13:59:05 +0200

    Revert "dynamic forms (#148)" (#156)
    This reverts the dynamic forms

--- src/main/java/com/dazzle/asklepios/service/dto/FormEntryCreateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.dazzle.asklepios.service.dto;
-
-public record FormEntryCreateDTO(
-        String title,
-        Long templateId,
-        Long facilityId,
-        Long departmentId,
-        String dataJson
-) {}
--- src/main/java/com/dazzle/asklepios/service/dto/FormEntryUpdateDTO.java
+++
@@ -1,6 +0,0 @@
-package com.dazzle.asklepios.service.dto;
-
-public record FormEntryUpdateDTO(
-        String title,
-        String dataJson
-) {}
