a64e16a3c6b86c371788496d21813e1870c7c384
bushramashaiekh <60824341+bushramashaiekh@users.noreply.github.com>
2026-01-07 13:58:04 +0200

    Revert "dynamic forms (#148)"
    This reverts commit 343940f518673599ea8a7671d0d52de03b3afbe5.

--- src/main/java/com/dazzle/asklepios/service/dto/FormTemplateUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.dazzle.asklepios.service.dto;
-
-public record FormTemplateUpdateDTO(
-        String name,
-        String description,
-        Long facilityId,
-        Long departmentId,
-        String formJson
-) {}
