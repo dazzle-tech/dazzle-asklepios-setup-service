786f8c55ef9a1846a0a90719d3a76ce24c676674
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-02-03 10:40:40 +0200

    enum for triage (#161)

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/patient/EmergencyLevel.java
@@ -0,0 +1,9 @@
+package com.dazzle.asklepios.domain.enumeration.patient;
+
+public enum EmergencyLevel {
+    LESS_URGENT,
+    NON_URGENT,
+    URGENT,
+    EMERGENT,
+    RESUSCITATION
+}
---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/patient/EncounterStatus.java
@@ -0,0 +1,16 @@
+package com.dazzle.asklepios.domain.enumeration.patient;
+
+public enum EncounterStatus {
+    NEW,
+    ONGOING,
+    CANCELED,
+    CLOSED,
+    DISCHARGED,
+    IN_OPERATION,
+    CONFIRM_RETURN,
+    TEMP_DC,
+    TRIAGE_STARTED,
+    SENT_TO_ER,
+    WAITING_TRIAGE,
+    WAITING_LIST
+}
