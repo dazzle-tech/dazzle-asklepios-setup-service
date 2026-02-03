2c244a97bdbd5c51ffda2a1eefa1d22154c733c1
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-02-03 10:35:34 +0200

    add getUsersByFacility endpoint (#145)
    * add getUsersByFacilities endpoint
    
    * Update UserRoleRepository.java
    
    * remove joins from repository

--- src/main/java/com/dazzle/asklepios/repository/UserRoleRepository.java
+++ src/main/java/com/dazzle/asklepios/repository/UserRoleRepository.java
@@ -1,9 +1,21 @@
 package com.dazzle.asklepios.repository;
+import com.dazzle.asklepios.domain.User;
 import com.dazzle.asklepios.domain.UserRole;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, UserRole.UserRoleId> {
     List<UserRole> findByIdUserId(Long userId);
     List<UserRole> findByIdRoleId(Long roleId);
+
+    @Query("""
+                select distinct ur.user
+                from UserRole ur
+                where ur.role.facilityId=:facilityId
+            """)
+    List<User> findDistinctUsersByFacilityId(Long facilityId);
+
 }
