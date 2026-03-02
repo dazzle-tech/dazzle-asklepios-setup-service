4eba846479f43293d49ab754d7c435ea39b3d28b
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-03-03 01:14:04 +0200

    edit enum

--- src/main/java/com/dazzle/asklepios/domain/enumeration/patient/SecurityLevel.java
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/patient/SecurityLevel.java
@@ -1,8 +1,8 @@
 package com.dazzle.asklepios.domain.enumeration.patient;
 
 public enum SecurityLevel {
-    Normal,
-    Confidential,
-    HighlyRestricted,
-    Locked
+    NORMAL_1,
+    CONFIDENTIAL_2,
+    HIGHLY_RESTRICTED_3,
+    LOCKED_4
 }
