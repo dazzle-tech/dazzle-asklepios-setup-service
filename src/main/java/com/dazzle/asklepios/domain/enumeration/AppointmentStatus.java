daa24325036beb75e540962fe35fe7c418c73fe5
bushramashaiekh <bushramashaie5@gmail.com>
2026-03-30 16:10:37 +0300

    enum from patient

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/AppointmentStatus.java
@@ -0,0 +1,13 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum AppointmentStatus {
+    NEW,
+    CONFIRMED,
+    CHECKED_IN,
+    IN_SERVICE,
+    COMPLETED,
+    CANCELLED,
+    NO_SHOW,
+    RESCHEDULED
+
+}
---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/SlotStrategy.java
@@ -0,0 +1,6 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum SlotStrategy {
+    FIXED_DURATION,
+    AS_DEPARTMENT_POOL
+}
