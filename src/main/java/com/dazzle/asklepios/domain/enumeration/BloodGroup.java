f4818c6b8c4df692b6f17c977c795e3a40c3d8c7
Qaisnfi7 <qais.farouk@gmail.com>
2026-04-29 15:56:02 +0300

    v1 (#223)

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/BloodGroup.java
@@ -0,0 +1,13 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum BloodGroup {
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE,
+    O_POSITIVE,
+    O_NEGATIVE,
+    UNKNOWN
+}
