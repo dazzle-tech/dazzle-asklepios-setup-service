10796ae13fb6442ed566ae2743e0b1e059bde9e9
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-06-22 15:22:57 +0300

    not

--- src/main/java/com/dazzle/asklepios/domain/enumeration/notification/NotificationCategory.java
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/notification/NotificationCategory.java
@@ -2,5 +2,10 @@ package com.dazzle.asklepios.domain.enumeration.notification;
 
 public enum NotificationCategory {
     APPOINTMENT_CONFIRMATION,
-    AUTHENTICATION
+    AUTHENTICATION,
+    QUICK_APPOINTMENT,
+    APPOINTMENT_RESCHEDULE,
+    APPOINTMENT_CANCELLATION,
+    MEDICAL_WARNING,
+    PAIN_ASSESSMENT
 }
--- src/main/java/com/dazzle/asklepios/domain/enumeration/notification/RecipientRule.java
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/notification/RecipientRule.java
@@ -3,8 +3,13 @@ package com.dazzle.asklepios.domain.enumeration.notification;
 public enum RecipientRule {
     PATIENT_EMAIL,
     PATIENT_PHONE,
+
     PRACTITIONER_EMAIL,
     PRACTITIONER_PHONE,
+    PRACTITIONER_USER,
+
+    DEPARTMENT_USERS,
+
     DATA,
     STATIC
 }
