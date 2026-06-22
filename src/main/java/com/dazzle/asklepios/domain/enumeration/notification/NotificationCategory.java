ac20ce5434a13f16a3d8945398957ab8758807a2
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-06-22 08:44:59 +0300

    Update NotificationCategory.java

--- src/main/java/com/dazzle/asklepios/domain/enumeration/notification/NotificationCategory.java
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/notification/NotificationCategory.java
@@ -1,9 +1,6 @@
 package com.dazzle.asklepios.domain.enumeration.notification;
 
 public enum NotificationCategory {
-    APPOINTMENT_REMINDER,
     APPOINTMENT_CONFIRMATION,
-    APPOINTMENT_CANCELLATION,
-    APPOINTMENT_RESCHEDULE,
     AUTHENTICATION
 }
