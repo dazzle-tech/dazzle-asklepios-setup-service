0d08e936d97c645eb4178b87c4b0f758167cf40d
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-07-26 10:52:30 +0300

    treatment enum

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/TreatmentStatus.java
@@ -0,0 +1,19 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum TreatmentStatus {
+    NEW,
+    ONGOING,
+    CANCELLED,
+    COMPLETED,
+    DISCHARGED,
+    IN_OPERATION,
+    CONFIRM_RETURN,
+    TEMP_DC,
+    TRIAGE_STARTED,
+    SENT_TO_ER,
+    WAITING_TRIAGE,
+    WAITING_LIST,
+    PENDING_PAYMENT,
+    ASSIGNED_TO_BED
+
+}
