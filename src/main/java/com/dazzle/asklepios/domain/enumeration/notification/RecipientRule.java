3e074ec54078e05cc9356f25562602fd608b6a87
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-08-17 14:19:57 +0300

    push

--- src/main/java/com/dazzle/asklepios/domain/enumeration/notification/RecipientRule.java
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/notification/RecipientRule.java
@@ -3,6 +3,7 @@ package com.dazzle.asklepios.domain.enumeration.notification;
 public enum RecipientRule {
     PATIENT_EMAIL,
     PATIENT_PHONE,
+    PATIENT_USER,
 
     PRACTITIONER_EMAIL,
     PRACTITIONER_PHONE,
