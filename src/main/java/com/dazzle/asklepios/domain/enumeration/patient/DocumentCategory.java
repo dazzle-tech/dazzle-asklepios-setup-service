561bd7c516d5383879f30604c5971704cc211ec2
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-08-25 13:46:55 +0300

    document

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/patient/DocumentCategory.java
@@ -0,0 +1,10 @@
+package com.dazzle.asklepios.domain.enumeration.patient;
+
+public enum DocumentCategory {
+    CONSENT,
+    QUESTIONNAIRE,
+    ASSESSMENT,
+    CLINICAL,
+    ADMINISTRATIVE,
+    OTHER
+}
