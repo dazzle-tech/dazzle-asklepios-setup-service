9b9b6ae6e3c7c5c808127f6144ffef1668e9c0ba
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-09-09 21:26:13 +0300

    v1 (#312)

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/analytics/Modules.java
@@ -0,0 +1,13 @@
+package com.dazzle.asklepios.domain.enumeration.analytics;
+
+public enum Modules {
+    APPOINTMENT,
+    PATIENT,
+    ENCOUNTER,
+    EMERGENCY,
+    URGENT_CARE,
+    LABORATORY,
+    RADIOLOGY,
+    PHARMACY,
+    BILLING
+}
