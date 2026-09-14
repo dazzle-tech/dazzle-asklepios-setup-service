688d815e55d34b7bb28bbbdf36cafc24fe327700
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-09-14 12:37:14 +0300

    Enums

---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/FLACCPainLevel.java
@@ -0,0 +1,8 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum FLACCPainLevel {
+    NO_PAIN,
+    MILD_PAIN,
+    MODERATE_PAIN,
+    SEVERE_PAIN
+}
---
+++ src/main/java/com/dazzle/asklepios/domain/enumeration/PainAssessmentTypes.java
@@ -0,0 +1,7 @@
+package com.dazzle.asklepios.domain.enumeration;
+
+public enum PainAssessmentTypes {
+    NUMERIC,
+    FACES,
+    FLACC
+}
